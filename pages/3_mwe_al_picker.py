73d9913b108e61310a6ceb017f57b45e51a78438
Peter-K-HSBI <peter.kuchling@hsbi.de>
2025-08-29 15:32:57 +0200

    insert game

--- pages/3_mwe_al_picker.py
+++ pages/3_mwe_al_picker.py
@@ -184,7 +184,7 @@ def uncert_choice(label,game):
     game.unc = dict_uncert[label]
     #game.update_uncert() #to be implemented
 
-def debug_printer():
+def debug_printer(game):
     st.write("It's working! It's working!")
 
 def main(classifier="forest",uncertainty="entropy", extra_params={}):
@@ -256,7 +256,7 @@ def main(classifier="forest",uncertainty="entropy", extra_params={}):
     plt.ylim(game.X_raw[:,1].min()-0.5,game.X_raw[:,1].max()+0.5)
     st.pyplot(game.fig)
     
-    st.button(label = "Recalc. Uncertainty", on_click = (lambda : debug_printer()))
+    st.button(label = "Recalc. Uncertainty", on_click = (lambda game: debug_printer(game)))
 
     #One final training with all queried points for evaluation
     #game.clf.fit(game.X_train, game.y_train)
